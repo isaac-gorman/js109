a9ff5c5d3139301a776cfa3d9b7177c146e4d037
Isaac Gorman <isaac@Isaacs-MacBook-Pro.local>
2021-07-17 12:45:23 -0500

    prog

--- answeringQuestions.js
+++
@@ -1,8 +0,0 @@
-let greeting = "Hello";
-
-while (true) {
-  greeting = "Hi";
-  break;
-}
-
-console.log(greeting);
---
+++ how2_answer/answeringQuestions.js
@@ -0,0 +1,32 @@
+let greeting = "Hello";
+
+while (true) {
+  greeting = "Hi";
+  break;
+}
+
+console.log(greeting);
+
+/*
+  Q: What will I be tested on when I am answering questions in the exam and interview?
+  
+  A: I will be tested on my understanding based on: 
+     - ability to analyze (parse a) program into its parts and describe each of their roles to precision
+
+     - my knowledge of a specific syntactical aspect or language specific feature in JavaScript 
+
+     - check my understanding of the deeper underlying principles; such as the fundamentals of the JavaScript language or a non-language specific programming concept.
+
+
+  Q: How should I answer questions? 
+
+  A: When answering questions I should: 
+   - Explain my reasoning with reference to specific lines in the program. Use line numbers to refer to particular lines of code when necessary. 
+
+   - Make sure to answer with extreme precision. Don't say function say function invocation or function definition when they are appropriate. Develop a Precise Language. 
+
+   - Highlight any specific syntactical conventions or technical observations where its is relevant.
+
+   - Identify the fundamental concept or concepts demonstrated by the question. 
+
+*/

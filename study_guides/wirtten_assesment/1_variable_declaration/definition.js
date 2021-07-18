81448a00e5912fcd1b4f96242c264347ac7580df
Isaac Gorman <isaac@Isaacs-MacBook-Pro.local>
2021-07-18 14:38:24 -0500

    prog

---
+++ study_guides/wirtten_assesment/1_variable_declaration/definition.js
@@ -0,0 +1,51 @@
+// Q: What is variable declaration?
+/* Answer:
+   - Variable declaration is when you first define a variable give it its name, and initialize it to a value.
+*/
+/*Textbook Definition:
+   - Variable declaration is a statement that asks the JavaScript engine to reserve space for a variable with a particular name. 
+
+   - Furthermore, the JS engine gives the option to specify an initial value for the variable.
+
+   - There are several ways to declare variables, but the preferred way in modern JavaScript uses the `let` keyword.
+
+
+*/
+/*w3school Definition: 
+  - Variable declaration is the the creation of a variable in JavaScript. 
+
+*/
+/*More Definitions: 
+  - JavaScript includes variables which hold data value and that value can change over time.
+  - JavaScript uses reserved keywords such as var, const, and let to declare a variable.
+
+  Q: Can I use variables in JS before using them?
+  - A: No. You have to declare variables "create them" before you can use them. The act of storing a value in variable is called "initialization"
+*/
+
+// Q: What is variable initialization?
+/* Answer:
+   - variable initialization is when you first declare a variable and assign it a value. Note that when ever a value is not explicitly assigned it will be initialized as undefined.
+*/
+/*More Definitions:
+    Q: So what does it mean to initialize a variable?
+    - A: Initialization is the action of storing a value (data) into a variable.
+
+    Q: What is variable initialization in JavaScript?
+    - A: Initialization is the means of assigning an initial value to a variable.
+
+    Q: What is variable initialization?
+    - A: Initializing a variable means specifying an initial value to assign it to (i.e. before it is used at all). Hence a variable that is not initialized does can not be used properly, and is actually implicitly initialized as undefined by the JavaScript engine.
+
+    Q: Why do we initialize variables in JavaScript?
+    - A: We explicity store values (data) in variables named by their identifiers to provide an idea of their intended use (and int)
+*/
+// Q: What is variable assignment?
+/* Answer:
+   - Variable assignment is the action of assigning a value to a variable using the assignment operator `=`. This can happen at anytime during the life time of a variable.
+*/
+
+// Q: What is variable re-assignment?
+/* Answer:
+   - Variable re-assignment is the act of giving a new value or assigning a new value to an existing variable with the use of the assignment operator `=`. 
+*/

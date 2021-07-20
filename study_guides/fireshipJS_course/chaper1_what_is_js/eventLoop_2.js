0b7989e335242879cf3f1380344ad3af8f5ff539
Isaac Gorman <isaac@Isaacs-MacBook-Pro.local>
2021-07-20 12:44:09 -0500

    event loop micro-queue

--- study_guides/fireshipJS_course/chaper1_what_is_js/eventLoop_2.js
+++ study_guides/fireshipJS_course/chaper1_what_is_js/eventLoop_2.js
@@ -11,6 +11,22 @@
 // - - Anything Async
 // - And I can do this with out blocking the main JS thread.
 
+// Deeper dive:
+// - That is almost everything I need to know about the event loop but JS had to make things a little more interesting by introducing "promises" and the "micro-task queue"
+// - Now I would think that with the promise would have two async operation with 0 delay and the promise resolve would be executed after the setTimeOut right? But nope ... not really bro.
+// - There is new mechanism called the microtask queue for promises which has priority over the main task queue used for DOM APIs, setTimeOuts and things like that. Which means that the handler for the promise will be called back first in this case.
+//  So in order to what is executed from first to last
+
+// Executed last
 setTimeout(() => console.log("do this first?"), 0);
 
+// Executed 2nd: Because the micro-task queue has priority whenever there is promise
+Promise.resolve().then((v) => console.log("do this second?"));
+
+// Executed 1st
 console.log("Let there be JavaScript!");
+
+// For Each round of the event loop:
+// 1. Run sync code
+// 2. Run Promise or microtask callbacks
+// 3. Run async task callbacks

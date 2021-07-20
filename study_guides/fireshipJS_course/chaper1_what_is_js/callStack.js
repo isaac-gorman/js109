2047020df1b9617886f9cd8c6f24bf390f409bf0
Isaac Gorman <isaac@Isaacs-MacBook-Pro.local>
2021-07-20 11:06:41 -0500

    prog

---
+++ study_guides/fireshipJS_course/chaper1_what_is_js/callStack.js
@@ -0,0 +1,15 @@
+function happy() {
+  const foo = "bar";
+  return ":)";
+}
+
+function mood(emoji) {
+  return emoji;
+}
+
+function currentStatus() {
+  console.log("howdy!");
+  return mood(happy());
+}
+
+currentStatus()

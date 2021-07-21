0154c5b64570a29ac1ea1ea660497a147f5afd9e
Isaac Gorman <isaac@Isaacs-MacBook-Pro.local>
2021-07-21 12:13:11 -0500

    lexical env

--- study_guides/fireshipJS_course/chapter2_JS_Dev/js_survival_guide.md
+++ study_guides/fireshipJS_course/chapter2_JS_Dev/js_survival_guide.md
@@ -43,3 +43,8 @@ A: A closure is the combination of a function bundled together (enclosed) with r
 
 Q: What is the lexical environment?
 A: Every time the JavaScript engine creates an execution context to execute the function or global code, it also creates a new lexical environment to store the variable defined in that function during the execution of that function. A lexical environment is a data structure that holds identifier-variable mapping.
+
+_Lexical environment of lexical context is the same thing as scope block._
+
+Q: But really what is closure?
+A: A lexical environment within a other lexical environment where the outer lexical environment's local variables remain available in memory after creation.
--- study_guides/fireshipJS_course/chapter2_JS_Dev/lexicalEnvironment.js
+++ study_guides/fireshipJS_course/chapter2_JS_Dev/lexicalEnvironment.js
@@ -8,8 +8,15 @@
 // A: The lexical environment is created behind the scenes and does not actually exist. It is a logistical concept. There is no way to actually access if from JavaScript code.
 
 // Q: How can you determine the structure of a program?
+// A: You can determine the structure of your program by using the scope brackets. 
+
+// Q: What is a conduit?
+// A: In electric engineering, a electric conduit is a tube used to protect and route electric wiring in a building or structure.
 
 // Q: How can I craft an entire applications structure that determines its data conduit?
+// A: Using scope brackets to separate lexical environments together with data types, functions, and variables definitions you can craft an entire application. 
+
+//  The scope or lexical environment in combination with var and let enable you to construct a data conduit supplying only particular data to specific actions and data structures.
 
 // Q: What is another way to term used for lexical context?
 

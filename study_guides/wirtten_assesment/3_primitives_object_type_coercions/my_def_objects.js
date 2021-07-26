b692de00ba8e3052dffbb6e7191c6e08194dad87
Isaac Gorman <emailme@isaacg.net>
2021-07-26 17:37:40 -0500

    memory data model - abstracted

---
+++ study_guides/wirtten_assesment/3_primitives_object_type_coercions/my_def_objects.js
@@ -0,0 +1,21 @@
+// Q: What are object types in JavaScript?
+// A: Everything that is not a primitive type in JS is an object data type.
+
+// Q: What is considered an object data type?
+// A: Objects, Arrays, and Function are actually all objects. Functions are objects too in fact we can set properties and methods on functions. Yes typeof in a function will return function but the Function constructor derives from the Object constructor.
+
+// Q: What are the two structural types?
+// A: The main two structural types in JavaScript are Object data structures and Function structures.
+
+// Q: What is the biggest difference between primitive types and objects?
+/* Answer:
+
+   - primitive types are immutable, objects only have an immutable reference, however an objects value can change over time. 
+   
+   - primitive types are passed by value. objects are passed by reference.
+
+   - primitive values are copied by value. Objects are copied by reference.
+
+   - primitive data types are compared by value. Objects are compared by reference or pointer
+
+*/

607c6f73876c215d45d8f0b2948ccdb8b0da9413
Isaac Gorman <emailme@isaacg.net>
2021-07-26 17:39:04 -0500

    variables

---
+++ study_guides/wirtten_assesment/1_variable_declaration/lydia.js
@@ -0,0 +1,19 @@
+// Variables:
+
+// Q: What is crucial in programming?
+// A: In programming nothing too cool can be accomplished without the ability to store values in some sort of identifiable container so you can use it throughout the program. In order to do this we use variables. In JavaScript you can declare a variable 3 different keywords: var, let, and const.
+
+// Q: When giving a variable a value what are we doing?
+// A: When giving a variable a value we are doing two things we are 1 declaring the variable with a unique identifier so we can call on it later and we are also assigning that variable a value.
+
+// Q: How do you initialize a variable?
+// A: You initialize a variable by explicitly assigning it a value.
+
+// Q: What is initializing?
+// A: Initializing is the act of assigning a value to a variable.
+
+// Q: When I declare and initialize a variables where is it stored?
+// Q: Well it depends. When JS files are executed in the runtime variables are store in either two places: the stack or the heap. The basic rule is primitive data types are stored on the stack and object types (array, object, and functions) are stored in the heap. The call stack is a memory allocation that executes our functions, and the memory heap is what stores complex data types such as (function, objects, and arrays) .
+
+// Article:
+// https://medium.com/@ethannam/javascripts-memory-model-7c972cd2c239

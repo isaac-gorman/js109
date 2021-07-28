95814be2e427d025c6a00a76fe0feba450b7831a
Isaac Gorman <emailme@isaacg.net>
2021-07-28 15:19:11 -0500

    functions are objects too!

---
+++ study_guides/wirtten_assesment/3_primitives_object_type_coercions/functions.js
@@ -0,0 +1,40 @@
+// Functions:
+
+// Q: In order to run certain blocks of code what do we need?
+// A: In order to execute certain code block we need functions. Whenever a function is invoked the code within it get executed. This enables to return something from the function although this is not always necessary. Some functions can return undefined if need be for the sake of limiting the complexity of functions.
+
+// Q: How can we defined a function?
+// A: A simple way to define a function is to prefix it with "function" keyword followed by the functions identifer (name). Then in between the curly brackets we can specify what we want function to do or what value to return. Function are input out put mechanisms.
+
+// Q: How can I invoke a function?
+// A: You can invoke a function by simply typing the  functions identifier (name) immediately  followed by closed parentheses.
+
+// Q: How can we pass arguments to a function?
+// A: I can also pass arguments to a function by adding values between the curly brackets. In order for the function to be able to receive arguments we need to specify it when declaring the function with the use of value placeholders that will inherit the values of the passed arguments within the code blocks of the function called parameters.
+
+// Q: What is the difference between an argument and parameter?
+// A: Function arguments are the real values  that the function will use in order to process some sort of output, and parameters are placeholder values used  within the function definition that get initilialized to the arguments values upon invokation.
+
+// Function Expressions
+
+// Q: What are the two types of function expressions?
+// A: There are two type of function expression: named, and anonymous. The difference is pretty straight forward, and gets clearer when you initialize a variable to a function.
+
+// Function Statements
+
+// Q: What is a function statement?
+// A: A function statement declares a function, and makes it possible for us to invoke the function at anytime.
+
+// Q: What is the difference between a function expression and function statements?
+// A: The function expression is assigned to a variable or initialized, and the function statements is the function that is on its own. This concepts will make a lot more sense once  I go over hoisting.
+
+// Arrow Functions
+
+// Q: Since when did we have a new way of writing functions?
+// A: Since the newest version of ECMAScript 2015 otherwise known as ES6 we have a more concise way to write functions.
+
+// Q: What is one benefit of arrow functions?
+// A: One of the many benefits of arrow functions is the ability they give use to write function on one line thus saving us a lot of space.
+
+// Q: What is another major benefit of the arrow function?
+// A: Another major benefit of using the arrow function comes into account when we are using objects and in reference to the "this" key word on methods.

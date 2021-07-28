4cbbacfe149520d298331c79e901807cf84cbe41
Isaac Gorman <emailme@isaacg.net>
2021-07-28 12:20:21 -0500

    arrays deep dive

---
+++ study_guides/wirtten_assesment/3_primitives_object_type_coercions/array.js
@@ -0,0 +1,60 @@
+// Arrays
+
+// Q: What can I store in any array?
+// A: You can store a sequence of any data type within an array
+// Examples of arrays:
+
+const fruit = ["apple", "banana", "orange"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const coordinates = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+// Q: What type of data are arrays used for storing?
+// A: Arrays are used to store the same type of data.
+
+// Q: What are array methods used for?
+// A: Array methods are tools we have as developers to interact with arrays in useful ways. Using existing methods on arrays we can loop over them (iterate through them), manipulate them, add or delete elements to them, and even extract data from them.
+
+// Q: What is the fastest way to access an element on an array?
+// A: Although array methods are useful the fastest way to access an element on an array is by index.
+// Example of accessing elements by index:
+// - I am accessing specific elements of the fruit array by using the arrays name followed by the bracket passing the bracket the index position I want.
+fruit[0]; // => "apple"
+fruit[1]; // => "banana"
+fruit[2]; // => "orange"
+
+// Q: What number do indexes of an arrays start out with?
+// A: Arrays are actually 0 index based. Meaning that the first element in the array is located at the index of 0.
+
+// Q: List out all the most commonly used array methods:
+/*
+
+   - .concat()
+   - .every()
+   - .fill()
+   - .filter()
+   - .find()
+   - .findIndex()
+   - .forEach()
+   - .indexOf()
+   - Array.isArray()
+   - .join()
+   - .lastIndexOf()
+   - .map()
+   - .pop()
+   - .push()
+   - .reduce()
+   - .reverse()
+   - .shift()
+   - .slice()
+   - .some()
+   - .sort()
+   - .splice()
+   - .toString()
+   - .unshift()
+ 
+
+*/

3423ca6b09f459d8e03bc02840d80635462cea70
Isaac Gorman <emailme@isaacg.net>
2021-07-28 20:50:07 -0500

    prog

--- study_guides/wirtten_assesment/3_primitives_object_type_coercions/typeCoercion.js
+++ study_guides/wirtten_assesment/3_primitives_object_type_coercions/typeCoercion.js
@@ -5,3 +5,16 @@
 // Q: What is complex about the about the type comparison table?
 
 // Q: What are my intentions of this annotation?
+// A:
+
+// Q: In general how should I approach type coercion?
+
+// Q: Why is else is it important to really understand type coercion?
+
+// Implicit vs Explicit Coercion
+
+// Q: What are the two types of coercion?
+
+// Q: What is considered explicit type coercion?
+
+// Q:
